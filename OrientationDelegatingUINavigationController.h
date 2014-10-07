221b441db2ed1de0d3d626d31d24f22d99451251
Patrice Brend'amour <dr.allgood@web.de>
2014-10-07 17:38:36 +0200

    initial version

---
+++ OrientationDelegatingUINavigationController.h
@@ -0,0 +1,13 @@
+//
+//  UINavigationController+Orientation.h
+//  SwitchControl_Framework
+//
+//  Created by Patrice Brend'amour on 10/7/14.
+//  Copyright (c) 2014 Patrice Brend'amour. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OrientationDelegatingUINavigationController : UINavigationController
+
+@end
---
+++ OrientationDelegatingUINavigationController.m
@@ -0,0 +1,41 @@
+//
+//  UINavigationController+Orientation.m
+//  SwitchControl_Framework
+//
+//  Created by Patrice Brend'amour on 10/7/14.
+//  Copyright (c) 2014 Patrice Brend'amour. All rights reserved.
+//
+
+#import "OrientationDelegatingUINavigationController.h"
+
+@implementation OrientationDelegatingUINavigationController
+
+- (BOOL)shouldAutorotate
+{
+    return [self.topViewController shouldAutorotate];
+}
+
+- (NSUInteger)supportedInterfaceOrientations
+{
+    return [self.topViewController supportedInterfaceOrientations];
+}
+
+- (UIInterfaceOrientation)preferredInterfaceOrientationForPresentation
+{
+    return [self.topViewController preferredInterfaceOrientationForPresentation];
+}
+
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 80000
+-(void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator {
+    if([self.topViewController respondsToSelector:@selector(viewWillTransitionToSize:withTransitionCoordinator:)]) {
+        [self.topViewController viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
+    }
+}
+#endif
+
+-(void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration {
+    if([self.topViewController respondsToSelector:@selector(willRotateToInterfaceOrientation:duration:)]) {
+        [self.topViewController willRotateToInterfaceOrientation:toInterfaceOrientation duration:duration];
+    }
+}
+@end
